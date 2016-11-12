f8ff30e720cec64bd943eb7919d4fd31c62a73f2
Bernadette <bernadette@englemaninvest.com>
2016-11-12 00:16:46 -0500

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -3,37 +3,36 @@ var d = new Date(),
     n = d.getFullYear();
 document.getElementById("year").innerHTML = n;
 
-// Variables
-var windowHeight=$(window).height(),
-    windowWidth=$(window).width(),
-    // Random Number
-    i,
-    // Quotes
-    msg = [
-        "A smile is your passport into the hearts of others.",
-        "What ever your goal is in life, embrace it, visualize it, and for it will be yours.",
-        "You cannot love life until you live the life you love.",
-        "The man or woman you desire feels the same about you.",
-        "Our deeds determine us, as much as we determine our deeds.",
-        "You can make your own happiness.",
-        "A stranger is a friend you have not spoken to yet.",
-        "A journey of a thousand miles begins with a single step.",
-        "Believing that you are beautiful will make you appear beautiful to others around you.",
-        "If you refuse to accept anything but the best, you very often get it.",
-        "Failure is only the opportunity to begin again more intelligently.",
-        "Integrity is doing the right thing, even if nobody is watching.",
-        "Conquer your fears or they will conquer you.",
-        "In this life it is not what we take up, but what we give up, that makes us rich.",
-        "A single conversation with a wise man is better than ten years of study.",
-        "The world may be your oyster, but that doesn't mean you'll get it's pearl.",
-        "Do not follow where the path may lead. Go where there is no path...and leave a trail.",
-        "We can't help everyone. But everyone can help someone.",
-        "All the water in the world can't sink a ship unless it gets inside.",
-        "Generosity will repay itself sooner than you imagine."];
+var windowHeight = $(window).height(),
+    windowWidth = $(window).width();
 
 // Call Quotes
 function fortune() {
-    var show = $('#show');
+    var show = $('#show'),
+        // Random Number
+        i,
+        // Quotes
+        msg = [
+            "A smile is your passport into the hearts of others.",
+            "What ever your goal is in life, embrace it, visualize it, and for it will be yours.",
+            "You cannot love life until you live the life you love.",
+            "The man or woman you desire feels the same about you.",
+            "Our deeds determine us, as much as we determine our deeds.",
+            "You can make your own happiness.",
+            "A stranger is a friend you have not spoken to yet.",
+            "A journey of a thousand miles begins with a single step.",
+            "Believing that you are beautiful will make you appear beautiful to others around you.",
+            "If you refuse to accept anything but the best, you very often get it.",
+            "Failure is only the opportunity to begin again more intelligently.",
+            "Integrity is doing the right thing, even if nobody is watching.",
+            "Conquer your fears or they will conquer you.",
+            "In this life it is not what we take up, but what we give up, that makes us rich.",
+            "A single conversation with a wise man is better than ten years of study.",
+            "The world may be your oyster, but that doesn't mean you'll get it's pearl.",
+            "Do not follow where the path may lead. Go where there is no path...and leave a trail.",
+            "We can't help everyone. But everyone can help someone.",
+            "All the water in the world can't sink a ship unless it gets inside.",
+            "Generosity will repay itself sooner than you imagine."];
 
     // Remove Currently Displayed Quote
     if(show != null) {
